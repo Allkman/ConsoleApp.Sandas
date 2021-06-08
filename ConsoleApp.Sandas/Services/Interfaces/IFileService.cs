f86fa7063c3b4ee9d1f7954e84470f974d175e36
Allkman <algirdas.cern@gmail.com>
2021-06-08 16:38:38 +0300

    Fixed interface

--- ConsoleApp.Sandas/Services/Interfaces/IFileService.cs
+++ ConsoleApp.Sandas/Services/Interfaces/IFileService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Sandas.Domain.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,6 @@ namespace ConsoleApp.Sandas.Services.Interfaces
 {
     public interface IFileService
     {
-        void ReturnTotalAmounts();
+        List<Employee> ReturnTotalAmounts();
     }
 }
